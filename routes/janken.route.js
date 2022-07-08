118c4b3f0183ae2ca1e9494c4352d26750ac77fc
ka-koyama <54408026+ka-koyama@users.noreply.github.com>
2022-07-09 03:11:31 +0900

    Delete routes directory

--- routes/janken.route.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getResult } from "../controllers/janken.controller.js";
-
-export const jankenRouter = express.Router();
-
-jankenRouter.post("/", (req, res) => getResult(req, res));
--- routes/omikuji.route.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getResult } from "../controllers/omikuji.controller.js"
-
-export const omikujiRouter = express.Router(); //exportと記載 = 他のファイルからimport可能
-
-omikujiRouter.get("/", (req, res) => getResult(req, res));
